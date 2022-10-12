8502a9ad60349bd15c2dc6c7bace8f59b4a2d338
Lars <larswalraevens@live.be>
2022-10-12 12:57:34 +0200

    typescript with webpack options

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    entry: "./src/index.ts", // Start processing code from this file
+    resolve: { // resolve typescript files, doesnt do this by default
+        extensions: ['.js', '.ts', '.tsx']
+    },
+    // register ts-loader for typescript files
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            exclude: /node-modules/,
+        }]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
