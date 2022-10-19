875f4f536e2a87a48a42c8ebb0d6a8b583d77299
Lars <larswalraevens@live.be>
2022-10-19 10:36:48 +0200

    Added pizzas & printed in list

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const message: string = "Hello World";
-
-const App = () => {
-    return <div>{message}</div>;
-}
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: "./src/index.tsx", // Start processing code from this file
+    devtool: "eval-source-map",
     resolve: { // resolve typescript files, doesnt do this by default
         extensions: ['.js', '.ts', '.tsx']
     },
